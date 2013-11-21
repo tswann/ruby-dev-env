eb9df55b4d88a785ea28de1b8266af2517a8b082
Thomas Swann <ts.swann@gmail.com>
2013-11-21 16:25:36 +0000

    init

---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#
+# tamthethird's Ruby Dev environment bootstrap script for vagrant
+# using Ubuntu precise32 standard box 
+#
+
+sudo apt-get update
+sudo apt-get install -y curl
+
+# Install RVM and update Ruby version
+\curl -L https://get.rvm.io | bash -s stable --ruby --autolibs=enable --auto-dotfiles
+
+rvm install 2.0.0
+rvm use 2.0.0 --default
+
+# Install Git and configure
+sudo apt-get install -y git
+
+# Setup development directories
+mkdir /home/SOURCE
