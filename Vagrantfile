4d9cd1baac547cfc230be2854cc35d5b38ac38f3
Thomas Swann <ts.swann@gmail.com>
2013-11-21 20:29:40 +0000

    Updated bootstrap and sync folders

--- Vagrantfile
+++ Vagrantfile
@@ -1,4 +1,5 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "precise32"
   config.vm.provision :shell, :path => "bootstrap.sh"
+  config.vm.synced_folder "src/", "/src"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -5,19 +5,19 @@
 # using Ubuntu precise32 standard box 
 #
 
-# Update packages 
+sudo -k
 sudo apt-get update
 sudo apt-get install -y curl
 
 # Install RVM and update Ruby version
 \curl -L https://get.rvm.io | bash -s stable --ruby --autolibs=enable --auto-dotfiles
-
 rvm install 2.0.0
 rvm use 2.0.0 --default
+
+# Install Heroku toolbelt
+wget -O- https://toolbelt.heroku.com/apt/release.key | apt-key add -
+sudo apt-get install -y heroku-toolbelt
 
 # Install Git and configure
 sudo apt-get install -y git
 cp /vagrant/gitconfig ~/.gitconfig
-
-# Setup development directories
-mkdir /home/SOURCE
